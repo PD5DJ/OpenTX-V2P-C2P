853fd9723c060785badd2c7c4a1500454fd9af33
Hobby4life <55426642+Hobby4life@users.noreply.github.com>
2020-11-03 12:29:30 +0100

    Update VLT%.lua

--- SCRIPTS/MIXES/VLT%.lua
+++ SCRIPTS/MIXES/VLT%.lua
@@ -34,8 +34,8 @@ local inputs     = { {"Instance", VALUE ,0,7,0}, {"Sensor", SOURCE}, {"Cells", V
 local wait_end   = 0
 local Cell_Count = 1
 local state
-local Voltage_Filtered = 0   
-local Battery_Connected          = 0 
+local Voltage_Filtered  = 0   
+local Battery_Connected = 0 
 
 --state functions forward declaration
 local wait, no_battery, done
@@ -52,7 +52,7 @@ function no_battery()
        wait_end   = getTime() + 200
    end  --  end if
 
-end  -- no_battery()
+end
 
 -------------------------------------------------------
 
@@ -63,7 +63,7 @@ function done()
        Battery_Connected    = 0
     end  -- end if
 
-end  -- end done()
+end
 
 -------------------------------------------------------
 
@@ -74,9 +74,7 @@ local function run(Instance,Voltage_Source, Cell_Count)
 
    -- the following table of percentages has 121 percentage values ,
    -- starting from 3.0 V to 4.2 V , in steps of 0.01 V 
-    --Voltage = Voltage_Source
-    --Cell_Count = Cells  
-  
+    
   local Percent_Table = 
 	{0  , 1  , 1  ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 ,  1 , 
 	 2  , 2  , 2  ,  2 ,  2 ,  2 ,  2 ,  2 ,  2 ,  2 ,  3 ,  3 ,  3 ,  3 ,  3 ,  3 ,  3 ,  3 ,  3 ,  3 , 
@@ -85,11 +83,11 @@ local function run(Instance,Voltage_Source, Cell_Count)
 	 33 , 36 , 39 , 42 , 45 , 48 , 51 , 54 , 57 , 58 , 60 , 62 , 64 , 66 , 67 , 69 , 70 , 72 , 74 , 75 , 
 	 77 , 78 , 80 , 81 , 82 , 84 , 85 , 86 , 86 , 87 , 88 , 89 , 91 , 92 , 94 , 95 , 96 , 97 , 97 , 99 , 100  }
 
-   Voltage_Filtered = Voltage_Filtered * 0.9  +  Voltage_Source * 0.1
+  Voltage_Filtered = Voltage_Filtered * 0.9  +  Voltage_Source * 0.1
    
-   if state == nil then state = no_battery end 
+  if state == nil then state = no_battery end 
    
-   if Cell_Count > 0 then 
+  if Cell_Count > 0 then 
 
     local Voltage_Cell    = 3
     local Battery_Percent = 0
@@ -106,7 +104,7 @@ local function run(Instance,Voltage_Source, Cell_Count)
 
     --https://opentx.gitbooks.io/opentx-2-2-lua-reference-guide/content/general/setTelemetryValue.html
     
-    setTelemetryValue(0x0310, 0, 0 + Instance, Battery_Percent * Battery_Connected, 13, 0, "VLT"..Instance)
+    setTelemetryValue(0x0310, 0, 0 + Instance, Battery_Percent, 13, 0, "VLT"..Instance)
     
     return
 
@@ -115,10 +113,6 @@ local function run(Instance,Voltage_Source, Cell_Count)
   return 0
 
 end
-
--------------------------------------------------------
-
----  OpenTX LUA interface parameters declared here
 
 return { run=run, input=inputs }
 
